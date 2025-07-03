bcd0efc86ad0bdbfb4e154f5d308dc5335587bf5
Gursahib Singh <aulakhg808@gmail.com>
2025-07-03 20:56:25 +0530

    React and vite

--- vite-learning/src/main.jsx
+++ vite-learning/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDom from "react-dom/client"
 
 import App from "./App";
 
+
+const reactElement = React.createElement(
+    
+)
+
 const root  = ReactDom.createRoot(document.getElementById('root')).render(
     <App/>
 )
