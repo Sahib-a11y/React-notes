556d5b9be6cc770084e9fbed53f8ec6963c0d465
Gursahib Singh <aulakhg808@gmail.com>
2025-07-04 14:45:55 +0530

    Vite and react

---
+++ 02.vite/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 5
+
+  const addvalue =() => {
+    if(counter < 20){
+    setCounter(counter+1)
+    console.log("clicked",counter);
+    }
+  }
+
+
+  const sub = ()=>{
+    if(counter > 0){
+    setCounter(counter-1)
+    console.log('clicked res', counter);
+    }
+  }
+
+
+  return (
+    <>
+    <h1>app file</h1> 
+    <h2>counter value : {counter} </h2>
+
+    <button onClick={addvalue}>add value</button> 
+    <br />
+    <button onClick={sub}>remove value</button>
+    </>
+  )
+}
+
+export default App
