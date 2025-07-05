afca2e2fe33821269ad530ef3735771616948c9c
Gursahib Singh <aulakhg808@gmail.com>
2025-07-05 16:32:47 +0530

    tailwind prop

---
+++ 03.tailwindprop/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { useState } from 'react'
+import Card from './components/card'
+
+function App() {
+  return(
+    <>
+    <h1>this is App</h1>
+    <Card username="dyaaan"/>
+    <Card username="iron man"/>
+    <Card username="tony stark"/>
+    <Card username="albert"/>
+    </>
+  )
+}
+
+export default App
